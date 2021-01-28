cf4eac221694053a11adb8fc3a7a9bb890ebd7a3
Bikram <bdasgupta@gmail.com>
2021-01-28 23:17:22 +0800

    Updates

---
+++ precache-manifest.918087634ec9b92bc6fe0c33379ea4a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d258c0f3c55398b9f1adf74eb2701990",
+    "url": "/99-frontend-junior-test/index.html"
+  },
+  {
+    "revision": "daf98e2f16e821170dcf",
+    "url": "/99-frontend-junior-test/static/css/main.0b89237a.chunk.css"
+  },
+  {
+    "revision": "52dbd628097530ed37d1",
+    "url": "/99-frontend-junior-test/static/js/2.5d98092f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/99-frontend-junior-test/static/js/2.5d98092f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf98e2f16e821170dcf",
+    "url": "/99-frontend-junior-test/static/js/main.c9583667.chunk.js"
+  },
+  {
+    "revision": "deee71094016561bda5c",
+    "url": "/99-frontend-junior-test/static/js/runtime-main.f9165181.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/99-frontend-junior-test/static/media/logo.ee7cd8ed.svg"
+  }
+]);
